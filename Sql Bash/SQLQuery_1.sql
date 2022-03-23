f175a5624a8d475815109ce552e978c62b952f86
Shockyks <shockyks@elfak.rs>
2022-03-23 12:32:27 +0100

    l

---
+++ Sql Bash/SQLQuery_1.sql
@@ -0,0 +1,20 @@
+SET IDENTITY_INSERT Aerodromi ON
+INSERT into Aerodromi (ID, Naziv, Grad, Drzava) VALUES (1, 'Nikola Tesla', 'Beograd', 'Srbija');
+INSERT into Aerodromi (ID, Naziv, Grad, Drzava) VALUES (2, 'Konstantin Veliki', 'Niš', 'Srbija');
+INSERT into Aerodromi (ID, Naziv, Grad, Drzava) VALUES (3, 'Ponikve', 'Užice', 'Srbija');
+INSERT into Aerodromi (ID, Naziv, Grad, Drzava) VALUES (4, 'El Prat', 'Barselona', 'Španija');
+INSERT into Aerodromi (ID, Naziv, Grad, Drzava) VALUES (5, 'Barajas', 'Madrid', 'Španija');
+INSERT into Aerodromi (ID, Naziv, Grad, Drzava) VALUES (6, 'Stansted', 'London', 'Engleska');
+INSERT into Aerodromi (ID, Naziv, Grad, Drzava) VALUES (7, 'Schwechat', 'Beč', 'Austrija');
+INSERT into Aerodromi (ID, Naziv, Grad, Drzava) VALUES (8, 'Newark', 'Njujork', 'SAD');
+INSERT into Aerodromi (ID, Naziv, Grad, Drzava) VALUES (9, 'Fiumicino', 'Rim', 'Italija');
+INSERT into Aerodromi (ID, Naziv, Grad, Drzava) VALUES (10, 'Charles de Gaulle', 'Pariz', 'Francuska');
+SET IDENTITY_INSERT Aerodromi OFF
+SET IDENTITY_INSERT Klase ON
+INSERT into Klase (ID, Naziv) VALUES (1, 'Ekonomska');
+INSERT into Klase (ID, Naziv) VALUES (2, 'Biznis');
+INSERT into Klase (ID, Naziv) VALUES (3, 'Prva');
+SET IDENTITY_INSERT Klase OFF
+
+
+
